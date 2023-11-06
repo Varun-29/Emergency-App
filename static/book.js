1abe61068c69e1460f4e07e60469c4b5c6fe2f2a
Varun S <121079552+Varun-29@users.noreply.github.com>
2023-11-06 11:36:03 +0530

    Update book.js

--- static/book.js
+++ static/book.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebas
 import { getAuth, } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB4IFyLYNlmu6eV2TJX8ES6xACydl2bzzU",
+    apiKey: "YOUR_API_KEY",
     authDomain: "coh-auth.firebaseapp.com",
     databaseURL: "https://coh-auth-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "coh-auth",
     storageBucket: "coh-auth.appspot.com",
     messagingSenderId: "94607897320",
-    appId: "1:94607897320:web:f7bc26674e162b9f5d4f5d",
+    appId: "YOUR_API_ID",
 };
 
 const app = initializeApp(firebaseConfig);
